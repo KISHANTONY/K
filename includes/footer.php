6b3ebb07ca703201353078bbbd0626719f7f5185
KISHAN <kishankktony@gmail.com>
2023-12-06 22:21:24 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-10">
+              <div class="footer-content">
+                <h4 style="color:#FFFFFF80;">
+                  FOLLOW US-
+                </h4>
+                  <a href="#"><p><span><i class="fab fa-facebook-f"></i></span> Facebook</p></a>
+                  <a href="#"><p><span><i class="fab fa-twitter"></i></span> Twitter</p></a>
+                  <a href="#"><p><span><i class="fab fa-instagram"></i></span> Instagram</p></a>
+                  <a href="#"><p><span><i class="fab fa-pinterest-p"></i></span> Pinterest</p></a>  
+              </div>
+            </div>
+            
+            <div class="col-2" >
+                <div class="footer-content">
+                  <h4 style="color:#FFFFFF80;">
+                    CONTACT-
+                  </h4>
+                    <p style="color:blue"><span><i class="fas fa-map-marker-alt"></i></span> Mookanoor,Angamaly</p>
+                    <a href="#"><p><span><i class="far fa-envelope"></i></span> fisat@gmail.com</p></a>
+                    <a href="#"><p><span><i class="fas fa-mobile-alt"></i></span> 02309240343</p></a>
+                    <a href="#"><p><span><i class="fas fa-phone"></i></span> 0487 235672</p></a>
+                </div>            
+              </div>
+          </div>
+          <hr>
+              <div class="footer-content">
+                <center>
+                <h6 style="color:#FFFFFF80;">
+                  Copyrights  Fisats5Cseb
+                </h6>
+                </center>
+              </div>
+
+        </div>
+        <!-- /.container -->
+    </footer>
